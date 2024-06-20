967ebc75dbd34753a633dfb2e66530fa50cc7d3e
Vladimir Sushentsev <cool.ya-vovas@yandex.ru>
2024-06-20 20:42:36 +0500

    Базовая реализация простейшего бота

--- text.py
+++ text.py
@@ -1,4 +1,15 @@
-print("hello")
-print("test")
-print("test3")
-print("home")
+from telebot import TeleBot
+bot = TeleBot('6234632894:AAFeXsjhbfMBrCkYsCiU-PQxzDW-a34Kots')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
