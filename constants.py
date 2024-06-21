8c10c89b2d46aa098ca24fcfccccdc97982b68e8
Vladimir Sushentsev <cool.ya-vovas@yandex.ru>
2024-06-21 21:55:10 +0500

    Реализация некоторых кнопок клавиатуры, инициализация файла с константами

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Базовые сообщения
+welcome_message = "Здравствуйте!\n\nЯ бот онлайн магазина «DwellWell». Пожалуйста, выберите, чем я могу вам помочь.";
+not_user_id = "Некому отправить сообщение(";
+incorrect_user_id = "Неверно введен ID пользователя!";
+
+# Константы для кнопок на клавиатуре
+keyboard_question = "Вопрос о товаре";
+keyboard_problem = "Проблема с товаром";
+keyboard_OZON_name = "Мы на OZON";
+keyboard_OZON_URL = "https://ozon.ru/t/d003na8";
+keyboard_WB_name = "Мы на Wildberries";
+keyboard_WB_URL = "";
+
+# Наименования callback-ов
+clbk_question = "question";
+clbk_problem = "problem";
+clbk_ozon = "ozon";
+clbk_wb = "wb";
+
+# Разделитель
+delimeter = "%";
--- text.py
+++ text.py
@@ -1,8 +1,12 @@
+import constants as const
+
 from telebot import TeleBot
 from telebot import types
 
 bot = TeleBot('6234632894:AAFeXsjhbfMBrCkYsCiU-PQxzDW-a34Kots')
 
+# ИЗМЕНИТЬ КЛАВИАТУРУ, ЧТОБ БЫЛО НЕ 4 КНОПКИ В КОЛОНКУ, А 2 КОЛОНКИ ПО 2 КНОПКИ
+# ДОБАВИТЬ ССЫЛКИ НА OZON, WB
 
 # @bot.message_handler(content_types=['text'])
 # def get_text_messages(message):
@@ -18,20 +22,46 @@ bot = TeleBot('6234632894:AAFeXsjhbfMBrCkYsCiU-PQxzDW-a34Kots')
 name = '';
 surname = '';
 age = 0;
+admin_id = 616381025;
+user_id = 0;
+
 @bot.message_handler(content_types=['text'])
 #Метод для обработки начала работы бота
 def start(message):
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, "Как тебя зовут?");
-        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
-    elif message.text == "/start":
-        bot.send_message(message.from_user.id, "Давай познакомимся\nКак тебя зовут?");
-        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
+    global user_id;
+
+    if message.from_user.id == admin_id:
+        str_splitted = message.text.split(const.delimeter); # Разделяю сообщение от админа на массив строк для возможности ввода сообщения формата 1234%текст,
+                                                #где "1234" - ID пользователя, которому нужно ответить, а "текст" - сообщение, отправленное ему
+        if len(str_splitted) > 1:
+            try:
+                id = int(str_splitted[0]);
+                bot.send_message(id, str_splitted[1]);
+            except Exception:
+                bot.send_message(admin_id, const.incorrect_user_id);
+        else:
+            if (user_id != 0):
+                bot.send_message(user_id, message.text);
+            else:
+                bot.send_message(admin_id, const.not_user_id);
     else:
-        bot.send_message(message.from_user.id, 'Напиши /reg');
+        if message.text == '/start':
+            user_id = message.from_user.id;
+            #Реализация через клавиатуру
+            keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+            key_question = types.InlineKeyboardButton(text=const.keyboard_question, callback_data=const.clbk_question); #кнопка «Да»
+            #keyboard.add(key_question); #добавляем кнопку в клавиатуру
+            key_problem= types.InlineKeyboardButton(text=const.keyboard_problem, callback_data=const.clbk_problem);
+            key_link_ozon= types.InlineKeyboardButton(text=const.keyboard_OZON_name, url = const.keyboard_OZON_URL, callback_data=const.clbk_ozon);
+            key_link_wb= types.InlineKeyboardButton(text=const.keyboard_WB_name, url = const.keyboard_WB_URL, callback_data=const.clbk_wb);
+            keyboard.row(key_question, key_problem);
+            keyboard.row(key_link_ozon, key_link_wb);
+            bot.send_message(message.from_user.id, text=const.welcome_message, reply_markup=keyboard)
+        else:
+            bot.send_message(message.from_user.id, 'Напиши /start');
 
 #Метод для считывая и запоминания имени
-def get_name(message): #получаем фамилию
+def get_name(message):
     global name;
     name = message.text;
     bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
@@ -49,9 +79,10 @@ def get_age(message):
     global age;
     while age == 0: #проверяем что возраст изменился
         try:
-             age = int(message.text) #проверяем, что возраст введен корректно
+            age = int(message.text) #проверяем, что возраст введен корректно
         except Exception:
-             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+            bot.register_next_step_handler(message, get_age);
     #Реализация через сообщение
     # answer = "Тебе "+str(age)+" лет, тебя зовут "+name+" "+surname+"?"
     # bot.send_message(message.from_user.id, answer)
@@ -66,13 +97,25 @@ def get_age(message):
     question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
+#Метод для отправки вопроса от пользователя к админу
+def get_question(message):
+    question = message.text;
+    formatted_question = "Вопрос от пользователя: "+ str(message.from_user.id) + "\n\n" + question;
+    bot.send_message(admin_id, formatted_question);
+    bot.register_next_step_handler(message, get_answer_for_question);
+
+#Метод для отправки ответа от админа пользователю
+def get_answer_for_question(message):
+    bot.send_message(message.from_user.id, message.text);
+    bot.register_next_step_handler(message, get_question);
+
 #Метод для подтверждения корректности введенных регистрационных данных
-def get_registration_answer(message):
-    if message.text.lower() == "да":
-        bot.send_message(message.from_user.id, "Отлично. Чем я могу тебе помочь, "+name)
-    elif message.text.lower() == "нет":
-        bot.send_message(message.from_user.id, "Тогда давай заново)\n\nКак тебя зовут?")
-        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
+# def get_registration_answer(message):
+#     if message.text.lower() == "да":
+#         bot.send_message(message.from_user.id, "Отлично. Чем я могу тебе помочь, "+name)
+#     elif message.text.lower() == "нет":
+#         bot.send_message(message.from_user.id, "Тогда давай заново)\n\nКак тебя зовут?")
+#         bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
 
 #Метод для обработки нажатий клавиш на клавиатуре
 @bot.callback_query_handler(func=lambda call: True)
@@ -83,6 +126,10 @@ def callback_worker(call):
     elif call.data == "no":
         bot.send_message(call.message.chat.id, "Тогда давай заново)\n\nКак тебя зовут?");
         bot.register_next_step_handler(call.message, get_name); #следующий шаг – функция get_name
-        bot.answer_callback_query(call.id, text="") #Устраняет проблему, что кнопка остается активной после нажатия на нее
+        bot.answer_callback_query(call.id, text="")
+    elif call.data == const.clbk_question:
+        bot.send_message(call.message.chat.id, "Пожалуйста, задайте мне свой вопрос. Я отправлю его менеджеру и перешлю ответ");
+        bot.register_next_step_handler(call.message, get_question); #следующий шаг – функция get_name
+        bot.answer_callback_query(call.id, text="")
 
 bot.polling(none_stop=True, interval=0)
